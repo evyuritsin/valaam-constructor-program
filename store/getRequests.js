850f595f2852cfc132ba929d40d732a7429b28d0
T G <ghalumyan@icloud.com>
2023-02-21 14:19:37 +0300

    fix fetchFirstStage

--- store/getRequests.js
+++ store/getRequests.js
@@ -6,21 +6,18 @@ const getRequests = {
 	getters: {},
 	actions: {
 		async fetchFirstStage(ctx, inputsData) {
-			const formData = new FormData()
-
-			formData.append('date_start', inputsData.arrivalDate)
-			formData.append('date_end', inputsData.departureDate)
-			formData.append('tourist_count', inputsData.guestsCount)
-			formData.append(
-				'dock_departure_id',
-				inputsData.departurePoint.start_dock_id
-			)
+			const formData = {
+				date_start: inputsData.arrivalDate,
+				date_end: inputsData.departureDate,
+				tourist_count: inputsData.guestsCount,
+				dock_departure_id: inputsData.departurePoint.start_dock_id,
+			}
 
 			const response = await fetch(
 				'http://valaamskiy-polomnik.directpr.beget.tech/api/constructor/firstStage',
 				{
 					method: 'POST',
-					body: { data: formData },
+					body: { data: JSON.stringify(formData) },
 				}
 			).then(r => r.json())
 			console.log(response)
