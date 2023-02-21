107ff4088d72e6e038c2c9f8726434554b7ce856
T G <ghalumyan@icloud.com>
2023-02-21 14:24:33 +0300

    fix fetchFirstStage

--- store/getRequests.js
+++ store/getRequests.js
@@ -13,11 +13,12 @@ const getRequests = {
 				dock_departure_id: inputsData.departurePoint.start_dock_id,
 			}
 
+			console.log(formData)
 			const response = await fetch(
 				'http://valaamskiy-polomnik.directpr.beget.tech/api/constructor/firstStage',
 				{
 					method: 'POST',
-					body: { data: JSON.stringify(formData) },
+					body: JSON.stringify({ data: formData }),
 				}
 			).then(r => r.json())
 			console.log(response)
